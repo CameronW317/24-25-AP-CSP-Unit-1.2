c79e3b07cde8dce8737b793df107360af3898992
15029 <cameron.weiss@vtsd.com>
2024-12-12 13:11:39 -0500

    finished

--- 1-2-5/main_game.py
+++ 1-2-5/main_game.py
@@ -7,15 +7,17 @@ wn = turtle.Screen()
 boxer_trtl = turtle.Turtle()
 score_writer = turtle.Turtle()
 counter = turtle.Turtle()
-message_tutle = turtle.Turtle()
+message_turtle = turtle.Turtle()
 
 weakpoint_shape = "circle"
 weakpoint_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
 boxer_shape = "giphy.gif"
 weakpoint_size = 2
-weakpoint_xcoor = [0, -200, -25, 25, -100, 0, -100]
+weakpoint_xcoor = [0, -175, -25, 25, -100, 0, -100]
 weakpoint_ycoor = [75, 50, 25, 25, -25, -25, -150]
 
+messages = ["WOW!", "NICE!", "EXCELLENT!", "CRITICAL!", "AMAZING!", "DESTRUCTIVE!", "INSANE!", "GREAT!", "POWERFUL!", "EXPLOSIVE!"]
+message_colors = ['red' , 'orange' , 'yellow' , 'green' , 'blue' , 'violet']
 score = 0
 
 font_setup = ("Arial", 20, "bold")
@@ -44,12 +46,15 @@ weakpoint.shapesize(weakpoint_size)
 weakpoint.penup()
 weakpoint.goto(0, 75)
 
+message_turtle.speed("fastest")
+message_turtle.hideturtle()
 
 wn.bgcolor("#A9A9A9")
 wn.addshape(boxer_shape)
 
 
 # Add opponent
+
 def draw_boxer(active_boxer):
   active_boxer.shape(boxer_shape)
   boxer_trtl.showturtle()
@@ -60,6 +65,7 @@ def update_score():
   score_writer.clear()
   score += 100
   score_writer.write(score, font=font_setup)
+
 def countdown():
   global timer, timer_up
   counter.clear()
@@ -70,34 +76,47 @@ def countdown():
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
     counter.getscreen().ontimer(countdown, counter_interval)
-def click_weakpoint():
+def click_weakpoint(x, y):
+
   global timer_up
   if timer_up == False:
     update_score()
-    countdown()
     draw_weakpoint()
+    hit_messages()
   else:
+    message_turtle.clear()
     weakpoint.hideturtle()
     weakpoint.penup()
     weakpoint.goto(-200, 0)
     weakpoint.pendown()
     boxer_trtl.hideturtle()
-    if score > 3000:
+    if score > 3500:
+      weakpoint.color("gold")
       weakpoint.write("You are a champion!", font=font_setup)
     else:
+      weakpoint.color("blue")
       weakpoint.write("You put up a good fight", font=font_setup)
+
 def draw_weakpoint():
   global weakpoint_xcoor, weakpoint_ycoor
   random_coordinate = random.randint(0,6)
   weakpoint.hideturtle()
   weakpoint.goto(weakpoint_xcoor[random_coordinate], weakpoint_ycoor[random_coordinate])
   weakpoint.showturtle()
+def hit_messages():
+
+  message_turtle.clear()
+  random_message = random.choice(messages)
+  random_color = random.choice(message_colors)
+  message_turtle.penup()
+  message_turtle.goto(random.randint(150, 300), random.randint(-300, 200))
+  message_turtle.color(random_color)
+  message_turtle.write(random_message, font=font_setup)
 
 # Incorporate weak points
 #def generate_weakpoint(xcoor, ycoor):
 
-
-
 draw_boxer(boxer_trtl)
 weakpoint.onclick(click_weakpoint)
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()
